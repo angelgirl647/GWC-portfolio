7ea140da4e15af3b0104d0b05dea5345dbf0f4f6
Yuki Fang <yfang4268@bths.edu>
2017-08-02 01:35:53 -0400

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,5 @@
 
-window.alert("WELCOME!");
+// window.alert("WELCOME!");
 
 //clicks counter
 var clicks = 0;
@@ -15,23 +15,41 @@ document.getElementById("robotPicsContainer").src="http://i.imgur.com/UHdxkp8.jp
 
 function changeBg(){
   click();
-  if (clicks==1){
-    document.body.style.backgroundColor="red";
+
+   if (clicks==1){
+
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white";
+    document.logo.style.color="orange";
+
 
   }
 
   else if (clicks==2){
-    document.body.style.backgroundColor="black";
-    document.body.style.color="white";
-    document.logo.style.color="white";
+    document.body.style.backgroundColor="white";
+    document.getElementById("motto").style.color="#ffa31a"; //orange
+    document.body.style.color="black";
+
 
   }
 
-  else{
-    document.body.style.backgroundColor="orange";
-    document.body.style.color="black";
-    document.body.motto.color="white"; 
+  else if (clicks==3){
+    document.body.style.backgroundColor="#ff944d"; //coral pink
+    document.getElementById("motto").style.color="red"
 
+  }
+
+  else if(clicks==4){
+
+    document.body.style.backgroundColor="red";
+    document.getElementById("motto").style.color="white"
+  }
+
+  else {
+    clicks=0;
+    document.body.style.backgroundColor="#ffa31a";  //orange
+    document.getElementById("motto").style.color="white";
+    document.body.style.color="black";
 
   }
 }
